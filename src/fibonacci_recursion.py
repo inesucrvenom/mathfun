cec435f908a19381fe66673c6883a0665bbd4c31
Ines <inesucrvenom@gmail.com>
2019-03-20 02:50:30 +0100

    implement fibonacci recursive with tests

---
+++ src/fibonacci_recursion.py
@@ -0,0 +1,21 @@
+"""Fibonacci"""
+
+
+def fibonacci(n: int) -> int:
+    """Implement Fibonacci(n) recursively"""
+
+    if type(n) is not int:
+        raise TypeError("n isn't integer")
+    if (n < 0):
+        raise ValueError("n is negative")
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fibonacci(n-1) + fibonacci(n-2)
+
+def my_handler(event, context):
+    """Lambda function for Fibonacci"""
+
---
+++ test_local/test_fibonacci_rec.py
@@ -0,0 +1,60 @@
+import unittest
+from src.fibonacci_recursion import fibonacci
+
+
+class TestFibonacciRecursive(unittest.TestCase):
+    """http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibtable.html"""
+
+    def test_low_index(self):
+        """Test for lower indices."""
+        self.assertEqual(0, fibonacci(0))
+        self.assertEqual(1, fibonacci(1))
+        self.assertEqual(1, fibonacci(2))
+        self.assertEqual(2, fibonacci(3))
+        self.assertEqual(3, fibonacci(4))
+        self.assertEqual(5, fibonacci(5))
+        self.assertEqual(8, fibonacci(6))
+        self.assertEqual(55, fibonacci(10))
+        self.assertEqual(28657, fibonacci(23))
+
+    def test_different_representation(self):
+        """Ints given in various forms"""
+        self.assertEqual(2584, fibonacci(0x12))   # f(18)
+        self.assertEqual(2584, fibonacci(0o22))   # f(18)
+        self.assertEqual(2584, fibonacci(0b10010))   # f(18)
+
+        # tuple of one int is int, whoa, that's new for me
+        self.assertEqual(3, fibonacci((4)))
+
+    def test_wrong_value(self):
+        """Check wrong inputs by value"""
+        with self.assertRaises(ValueError): fibonacci(-2)
+        with self.assertRaises(ValueError): fibonacci((-2))
+
+    def test_wrong_type(self):
+        """check wrong inputs by type"""
+        # looks like int
+        with self.assertRaises(TypeError): fibonacci(3.0)
+        with self.assertRaises(TypeError): fibonacci(0.0)
+        with self.assertRaises(TypeError): fibonacci(-23.0)
+
+        # floats
+        with self.assertRaises(TypeError): fibonacci(3.5)
+        with self.assertRaises(TypeError): fibonacci(-2.5)
+
+        # other objects
+        with self.assertRaises(TypeError): fibonacci("a")
+
+        with self.assertRaises(TypeError): fibonacci([])
+        with self.assertRaises(TypeError): fibonacci([4])
+        with self.assertRaises(TypeError): fibonacci([4, 3])
+
+        with self.assertRaises(TypeError): fibonacci({})
+        with self.assertRaises(TypeError): fibonacci({10: 40})
+
+        with self.assertRaises(TypeError): fibonacci(())
+        with self.assertRaises(TypeError): fibonacci((2, 3))
+
+
+if __name__ == '__main__':
+    unittest.main()
