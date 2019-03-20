d84b96cbb1d33e7a4ecd6fcb8485ba13bd42e43b
Ines <inesucrvenom@gmail.com>
2019-03-20 08:39:15 +0100

    refactor tests for better readability

--- test_local/test_fibonacci_recursive.py
+++ test_local/test_fibonacci_recursive.py
@@ -3,12 +3,13 @@ http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibtable.html
 """
 
 import unittest
-from src.fibonacci_recursive import fibonacci
+from src.lambda_fibonacci_recursive import fibonacci
 
 
-class Test_Logic_FibonacciRecursive(unittest.TestCase):
-    def test_low_index(self):
-        """Test for lower indices."""
+class Test_Fibonacci_Recursive(unittest.TestCase):
+    """Test recursive implementation of Fibonacci
+    """
+    def test_correctness_valueForSmallIndices(self):
         self.assertEqual(0, fibonacci(0))
         self.assertEqual(1, fibonacci(1))
         self.assertEqual(1, fibonacci(2))
@@ -19,43 +20,44 @@ class Test_Logic_FibonacciRecursive(unittest.TestCase):
         self.assertEqual(55, fibonacci(10))
         self.assertEqual(28657, fibonacci(23))
 
-    def test_different_representation(self):
-        """Ints given in various forms."""
-        self.assertEqual(2584, fibonacci(0x12))   # f(18)
-        self.assertEqual(2584, fibonacci(0o22))   # f(18)
-        self.assertEqual(2584, fibonacci(0b10010))   # f(18)
+    def test_correctness_differentIntegerRepresentation_forInt18(self):
+        self.assertEqual(2584, fibonacci(0x12))
+        self.assertEqual(2584, fibonacci(0o22))
+        self.assertEqual(2584, fibonacci(0b10010))
 
-        # tuple of one int is int, whoa, that's new for me :-o
-        self.assertEqual(3, fibonacci((4)))
-
-    def test_wrong_value(self):
-        """Check wrong inputs by value."""
+    def test_wrongValue_negativeIntegers(self):
         with self.assertRaises(ValueError): fibonacci(-2)
         with self.assertRaises(ValueError): fibonacci((-2))
 
-    def test_wrong_type(self):
-        """Check wrong inputs by type."""
-        # those who looks like int
+    def test_wrongType_lookLikeInteger(self):
         with self.assertRaises(TypeError): fibonacci(3.0)
         with self.assertRaises(TypeError): fibonacci(0.0)
         with self.assertRaises(TypeError): fibonacci(-23.0)
 
-        # obvious floats
+    def test_wrongType_obviousFloat(self):
         with self.assertRaises(TypeError): fibonacci(3.5)
         with self.assertRaises(TypeError): fibonacci(-2.5)
 
-        # other objects
+    def test_wrongType_string(self):
         with self.assertRaises(TypeError): fibonacci("a")
 
+    def test_wrongType_array(self):
         with self.assertRaises(TypeError): fibonacci([])
         with self.assertRaises(TypeError): fibonacci([4])
         with self.assertRaises(TypeError): fibonacci([4, 3])
 
+    def test_wrongType_dictionary(self):
         with self.assertRaises(TypeError): fibonacci({})
         with self.assertRaises(TypeError): fibonacci({10: 40})
 
+    def test_wrongType_tuple(self):
         with self.assertRaises(TypeError): fibonacci(())
+        with self.assertRaises(TypeError): fibonacci((1,))
         with self.assertRaises(TypeError): fibonacci((2, 3))
+
+    def test_correctness_fakeTupleOfOne(self):
+        # this is not a tuple, but int; tuple is defined by comma
+        self.assertEqual(3, fibonacci((4)))
 
 
 if __name__ == '__main__':
