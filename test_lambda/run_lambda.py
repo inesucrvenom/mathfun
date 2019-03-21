dd21e1a555354655b030bb52b8e0617221dad500
Ines <inesucrvenom@gmail.com>
2019-03-21 04:57:06 +0100

    improve invoke_lambda to distinguish errors from invalid user input and others

--- test_lambda/run_lambda.py
+++ test_lambda/run_lambda.py
@@ -1,5 +1,4 @@
 """ Generic call for lambda functions in src/
-Implement custom exception for better UI.
 
 Each lambda has to take care of its own validation.
 Each lambda returns either function int value or error message.
@@ -14,8 +13,11 @@ def invoke_lambda(lambda_name: str, parameters: dict) -> (int, int):
     """Call lambda on AWS with given parameters.
     Return evaluation result as int.
 
-    Propagate TypeError and ValueError from invoked lambda.
-    Raise TypeError if result isn't already explained by lambda result.
+    Errors include message from the lambda, for improving UX.
+    - TypeError and ValueError from invoked lambda, propagate.
+    - raise TypeError if result isn't already explained by lambda result.
+    - RuntimeError for timeout and similar errors that come without type.
+    - re-raise Exception for all other cases.
     """
     client = boto3.client('lambda')
 
@@ -32,14 +34,25 @@ def invoke_lambda(lambda_name: str, parameters: dict) -> (int, int):
     try:
         result_value = int(result_json)
 
-    except TypeError as unspecified:
-        # propagate exact error message sent by lambda
-        if result_json["errorType"] == "TypeError":
-            raise TypeError(result_json["errorMessage"]) from unspecified
-        elif result_json["errorType"] == "ValueError":
-            raise ValueError(result_json["errorMessage"]) from unspecified
+    # distinguish and propagate exact errors from lambda
+    # to filter out invalid user input from other errors
+    except TypeError as type_err:
+        error_message = result_json["errorMessage"]
+        try:
+            type = result_json["errorType"]
+        except KeyError:
+            # e.g. timeout
+            raise RuntimeError(error_message)
         else:
-            raise Exception(result_json) from unspecified
+            # lambda invalid input
+            if type == "TypeError":
+                raise TypeError(error_message) from type_err
+            elif type == "ValueError":
+                raise ValueError(error_message) from type_err
+            else:
+                raise Exception(error_message) from type_err
+    except Exception as unk_err:
+        raise Exception(str(result_json)) from unk_err
 
     else:
         return result_value
