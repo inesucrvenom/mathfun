2542baae74ad6eefb7c40bff96b10a30e1e3355a
Ines <inesucrvenom@gmail.com>
2019-03-21 07:23:38 +0100

    add new examples

--- manual_run.py
+++ manual_run.py
@@ -1,5 +1,6 @@
 from test_lambda.run_lambda import show_result
 
+
 def run_all():
     """Create own lambda invocations"""
 
@@ -12,13 +13,19 @@ def run_all():
     # print(show_result('lambda_fibonacci_recursive', {'n': {}}))
     # print(show_result('lambda_fibonacci_recursive', {'n': 31}))
 
-    print(show_result('lambda_fibonacci_iterative', {'n': 2}))
-    print(show_result('lambda_fibonacci_iterative', {'n': -2}))
-    print(show_result('lambda_fibonacci_iterative', {'n': "a"}))
-    print(show_result('lambda_fibonacci_iterative', {'k': 2}))
-    print(show_result('lambda_fibonacci_iterative', {'n': {}}))
-    print(show_result('lambda_fibonacci_iterative', {'n': 31}))
+    # print(show_result('lambda_fibonacci_iterative', {'n': 2}))
+    # print(show_result('lambda_fibonacci_iterative', {'n': -2}))
+    # print(show_result('lambda_fibonacci_iterative', {'n': "a"}))
+    # print(show_result('lambda_fibonacci_iterative', {'k': 2}))
+    # print(show_result('lambda_fibonacci_iterative', {'n': {}}))
+    # print(show_result('lambda_fibonacci_iterative', {'n': 31}))
 
+    print(show_result('lambda_factorial_recursive', {'n': 2}))
+    print(show_result('lambda_factorial_recursive', {'n': -2}))
+    print(show_result('lambda_factorial_recursive', {'n': "a"}))
+    print(show_result('lambda_factorial_recursive', {'k': 2}))
+    print(show_result('lambda_factorial_recursive', {'n': {}}))
+    print(show_result('lambda_factorial_recursive', {'n': 15}))
 
 
 if __name__ == '__main__':
