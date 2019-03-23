c97343cff7ed22d32302d73e415c5610bd5ed66d
Ines <inesucrvenom@gmail.com>
2019-03-23 06:13:52 +0100

    clean up to leave just basic run examples

--- manual_run.py
+++ manual_run.py
@@ -4,37 +4,12 @@ from test.run_lambda import show_result
 def run_all():
     """Create own lambda invocations"""
 
-    # print(show_result('lambda_fibonacci_recursive', {'n': 2}))
-    # print(show_result('lambda_fibonacci_recursive', {'n': -2}))
-    # print(show_result('lambda_fibonacci_recursive', {'n': "a"}))
-    # print(show_result('lambda_fibonacci_recursive', {'k': 2}))
-    # print(show_result('lambda_fibonacci_recursive', {'n': {}}))
-    print(show_result('lambda_fibonacci_recursive', {'n': 31}))
+    print(show_result('lambda_fibonacci_recursive', {'n': 20}))
+    print(show_result('lambda_fibonacci_iterative', {'n': 20}))
 
-    # print(show_result('lambda_fibonacci_iterative', {'n': 2}))
-    # print(show_result('lambda_fibonacci_iterative', {'n': -2}))
-    # print(show_result('lambda_fibonacci_iterative', {'n': "a"}))
-    # print(show_result('lambda_fibonacci_iterative', {'k': 2}))
-    # print(show_result('lambda_fibonacci_iterative', {'n': {}}))
-    # print(show_result('lambda_fibonacci_iterative', {'n': 31}))
+    print(show_result('lambda_factorial_recursive', {'n': 20}))
+    print(show_result('lambda_factorial_iterative', {'n': 20}))
 
-    # print(show_result('lambda_factorial_recursive', {'n': 2}))
-    # print(show_result('lambda_factorial_recursive', {'n': -2}))
-    # print(show_result('lambda_factorial_recursive', {'n': "a"}))
-    # print(show_result('lambda_factorial_recursive', {'k': 2}))
-    # print(show_result('lambda_factorial_recursive', {'n': {}}))
-    # print(show_result('lambda_factorial_recursive', {'n': 15}))
-    #
-    # print(show_result('lambda_factorial_recursive', {'n': 994}))
-
-    print(show_result('lambda_factorial_iterative', {'n': 2}))
-    print(show_result('lambda_factorial_iterative', {'n': -2}))
-    print(show_result('lambda_factorial_iterative', {'n': "a"}))
-    print(show_result('lambda_factorial_iterative', {'k': 2}))
-    print(show_result('lambda_factorial_iterative', {'n': {}}))
-    print(show_result('lambda_factorial_iterative', {'n': 15}))
-
-    print(show_result('lambda_factorial_iterative', {'n': 45000}))
 
 if __name__ == '__main__':
     run_all()
