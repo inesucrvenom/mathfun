f02d737bb6e6b39ea0894543b8a445dbc13282c3
Ines <inesucrvenom@gmail.com>
2019-03-23 05:09:48 +0100

    extract part for raising exceptions after filtering out some types of errors

--- test/run_lambda.py
+++ test/run_lambda.py
@@ -14,13 +14,11 @@ def invoke_lambda(lambda_name: str, parameters: dict) -> (int, int):
     Return evaluation result as int.
 
     Errors include message from the lambda, for improving UX.
-    - TypeError and ValueError from invoked lambda, propagate.
-    - raise TypeError if result isn't already explained by lambda result.
-    - RuntimeError for timeout and similar errors that come without type.
+    - get TypeError and ValueError from invoked lambda
+    - get Exception when result isn't already explained by lambda result.
     - re-raise Exception for all other cases.
     """
     client = boto3.client('lambda')
-
     response = client.invoke(
         FunctionName=lambda_name,
         LogType='Tail',
@@ -33,28 +31,34 @@ def invoke_lambda(lambda_name: str, parameters: dict) -> (int, int):
 
     try:
         result_value = int(result_json)
-
-    # distinguish and propagate exact errors from lambda
-    # to filter out invalid user input from other errors
-    except TypeError as type_err:
-        error_message = result_json["errorMessage"]
-        try:
-            type = result_json["errorType"]
-        except Exception as err:
-            raise Exception(error_message) from err
-        else:
-            # lambda invalid input
-            if type == "TypeError":
-                raise TypeError(error_message) from type_err
-            elif type == "ValueError":
-                raise ValueError(error_message) from type_err
-            else:
-                raise Exception(error_message) from type_err
-    except Exception as unk_err:
-        raise Exception(str(result_json)) from unk_err
-
+    except TypeError as err_json_not_int:
+        distinguish_user_errors_from_others(result_json, err_json_not_int)
+    except Exception as err_unknown:
+        raise Exception(str(result_json)) from err_unknown
     else:
         return result_value
+
+
+def distinguish_user_errors_from_others(result_json, err_json_not_int):
+    """ Propagate exact errors based on type field, if it's available.
+    Filtering out invalid user input from other errors.
+
+    - Propagate TypeError and ValueError from invoked lambda
+    - Raise Exception when no type found.
+    """
+    error_message = result_json["errorMessage"]
+    try:
+        err_type_user_input = result_json["errorType"]
+    except Exception as err_unknown:
+        raise Exception(error_message) from err_unknown
+    else:
+        # Distinguish invalid user input in lambda.
+        if err_type_user_input == "TypeError":
+            raise TypeError(error_message) from err_json_not_int
+        elif err_type_user_input == "ValueError":
+            raise ValueError(error_message) from err_json_not_int
+        else:
+            raise Exception(error_message) from err_json_not_int
 
 
 def show_result(function_name: str, parameters: dict) -> str:
@@ -70,8 +74,8 @@ def show_result(function_name: str, parameters: dict) -> str:
     try:
         result = invoke_lambda(function_name, parameters)
     except Exception as e:
-        message = message + "is not computable\n" + str(e)
+        message += "is not computable\n" + str(e)
     else:
-        message = message + "= {}".format(result)
+        message += "= {}".format(result)
 
     return message
